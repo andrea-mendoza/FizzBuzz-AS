f603353589b996b08a995f9460ddbae7b78b043b
andymehi <andymehi@hotmail.com>
2019-02-10 20:50:51 -0400

    Fizz Test

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -5,7 +5,12 @@ var fb = require("../fizzbuzz.js");
 
 
 describe('FizzBuzz', function(){
+
     it('Cuando el numero es 1 deberia devolver 1',function(){
         expect(fb.fizzbuzz()[1]).equal("1");
+    });
+
+    it('Cuando el numero es 3 deberia devolver Fizz',function(){
+        expect(fb.fizzbuzz()[3]).equal("Fizz");
     });
 });
