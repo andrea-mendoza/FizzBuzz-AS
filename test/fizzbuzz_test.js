adbbe10c0e1406ff9267e40d8c9cee60921e2f51
andymehi <andymehi@hotmail.com>
2019-02-10 21:58:51 -0400

    Refactoring

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -2,23 +2,27 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var should = require('chai').should();
 var fb = require("../fizzbuzz.js");
-
+var vectorFizzBuzz = [];
 
 describe('FizzBuzz', function(){
 
+    before(function() {
+        vectorFizzBuzz =  fb.fizzbuzz();
+    });
+    
     it('Cuando el numero es 1 deberia devolver 1',function(){
-        expect(fb.fizzbuzz()[1]).equal("1");
+        expect(vectorFizzBuzz[1]).equal("1");
     });
 
     it('Cuando el numero es 3 deberia devolver Fizz',function(){
-        expect(fb.fizzbuzz()[3]).equal("Fizz");
+        expect(vectorFizzBuzz[3]).equal("Fizz");
     });
 
     it('Cuando el numero es 5 deberia devolver Buzz',function(){
-        expect(fb.fizzbuzz()[5]).equal("Buzz");
+        expect(vectorFizzBuzz[5]).equal("Buzz");
     });
 
     it('Cuando el numero es 15 deberia devolver FizzBuzz',function(){
-        expect(fb.fizzbuzz()[15]).equal("FizzBuzz");
+        expect(vectorFizzBuzz[15]).equal("FizzBuzz");
     });
 });
