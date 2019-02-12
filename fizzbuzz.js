6916dc98a459e4a627f46552d5a3613de3010a58
andymehi <andymehi@hotmail.com>
2019-02-12 08:58:59 -0400

    Generar Valor Tests

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,28 +1,30 @@
+function generarValor(numero){
+  var valor;
+  if(numero % 15 === 0){
+    valor = "FizzBuzz";
+  }
+  else if(numero % 3 == 0){
+    valor = "Fizz";
+  }
+  else if(numero % 5 === 0){
+    valor = "Buzz";
+  }
+  else{
+    valor = numero.toString();
+  }
+  return valor;
+}
 
 function fizzbuzz(){
-  var retorna = [];
+  var auxFizzBuzz = [];
   for(cont = 0; cont<100;cont++){
-    if(cont % 15 === 0){
-      retorna[cont] = "FizzBuzz";
-      console.log("FizzBuzz");
-    }
-    else if(cont % 3 == 0){
-      retorna[cont] = "Fizz";
-      console.log("Fizz");
-    }
-    else if(cont % 5 === 0){
-      retorna[cont] = "Buzz";
-      console.log("Buzz");
-    }
-    else{
-      retorna[cont] = cont.toString();
-      console.log(cont);
-    }
+    auxFizzBuzz[cont] = generarValor(cont);
   }
-  return retorna;
+  return auxFizzBuzz;
 }
 
 module.exports = {
-  fizzbuzz: fizzbuzz
+  fizzbuzz: fizzbuzz,
+  generarValor: generarValor
 }
 
--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -5,24 +5,25 @@ var fb = require("../fizzbuzz.js");
 var vectorFizzBuzz = [];
 
 describe('FizzBuzz', function(){
-
+    
     before(function() {
         vectorFizzBuzz =  fb.fizzbuzz();
     });
-    
-    it('Cuando el numero es 1 deberia devolver 1',function(){
-        expect(vectorFizzBuzz[1]).equal("1");
+
+    it('Cuando el numero es multiplo de 3, se deberia generar Fizz',function(){
+        expect(fb.generarValor(3)).equal("Fizz");
     });
 
-    it('Cuando el numero es 3 deberia devolver Fizz',function(){
-        expect(vectorFizzBuzz[3]).equal("Fizz");
+    it('Cuando el numero es multiplo 5, se deberia generar Buzz',function(){
+        expect(fb.generarValor(5)).equal("Buzz");
     });
 
-    it('Cuando el numero es 5 deberia devolver Buzz',function(){
-        expect(vectorFizzBuzz[5]).equal("Buzz");
+    it('Cuando el numero es multiplo 15, se deberia generar FizzBuzz',function(){
+        expect(fb.generarValor(15)).equal("FizzBuzz");
     });
 
-    it('Cuando el numero es 15 deberia devolver FizzBuzz',function(){
-        expect(vectorFizzBuzz[15]).equal("FizzBuzz");
+    it('Cuando el numero no es multiplo de 3,5 o 15, se deberia generar el mismo numero',function(){
+        expect(fb.generarValor(11)).equal("11");
     });
+
 });
